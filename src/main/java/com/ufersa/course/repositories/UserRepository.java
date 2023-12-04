57d50d52eb1555f30d3e0f5bc4a37428ec9fced7
Samuel <rogenescarvalho@hotmail.com>
2023-12-04 14:04:20 -0300

    JPA repository

---
+++ src/main/java/com/ufersa/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ufersa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ufersa.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
