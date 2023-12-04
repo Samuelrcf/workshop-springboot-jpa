66cf37d51a47db696e3170a7caba6a646ab6eacf
Samuel <rogenescarvalho@hotmail.com>
2023-12-04 14:21:48 -0300

    Service layer

--- src/main/java/com/ufersa/course/resources/UserResource.java
+++ src/main/java/com/ufersa/course/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.ufersa.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ufersa.course.entities.User;
+import com.ufersa.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 		
-		User user = new User(1L ,"James", "james@gmail.com", "0098765687", "123456");
-		return ResponseEntity.ok().body(user);
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
